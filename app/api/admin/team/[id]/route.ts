3d5dcd4c6f36363ef41f29522408431e2e0531ee
daryappa8018 <daryappamane098@gmail.com>
2026-01-01 14:05:40 +0530

    Added dynamic export to API routes

--- app/api/admin/team/[id]/route.ts
+++ app/api/admin/team/[id]/route.ts
@@ -6,6 +6,8 @@ import { prisma } from '@/lib/prisma';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
+export const dynamic = 'force-dynamic';
+
 // GET single team member
 export async function GET(
   request: NextRequest,
