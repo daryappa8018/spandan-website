9c1431f302e0b2498e128adecd6abe1187489242
daryappa8018 <daryappamane098@gmail.com>
2026-01-12 10:47:32 +0530

    Add gallery components index for easier imports

---
+++ components/gallery/index.ts
@@ -0,0 +1,10 @@
+// components/gallery/index.ts
+// Central export for all gallery components
+
+export { default as PhotoCarousel } from './PhotoCarousel';
+export { default as GalleryGrid } from './GalleryGrid';
+export { default as PhotoLightbox } from './PhotoLightbox';
+export { default as GalleryFilters } from './GalleryFilters';
+
+// Type exports
+export type { FilterOptions } from './GalleryFilters';
